bce883c8545fa007c9688b7048031e460f63b231
OUALID AAZAB <160775667+Oualidaazab@users.noreply.github.com>
2026-05-06 00:13:46 +0200

    Update message sent to server in cleint.py

--- cleint_server/cleint.py
+++ cleint_server/cleint.py
@@ -3,5 +3,5 @@ host ='127.0.1.1'
 PORT =9090 #  port  number 
 socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM) 
 socket.connect((host,PORT)) 
-socket.send("hello my niga  ".encode('utf-8')) 
+socket.send("hello how are you doing  :   ".encode('utf-8')) 
 print(socket.recv(1024))
